4c239e8057e8b0d04f3e97fcc10ee670ad79c7e0
Awadje <michaelawad85@gmail.com>
2017-06-18 12:50:10 +0200

    Search Bar hooked up with SWAPI

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,11 +5,11 @@ const ROOT_URL = 'https://swapi.co/api/people/';
 export const FETCH_SWAPI = 'FETCH_SWAPI';
 
 export function fetchSwapi(name) {
-  const url = `${ROOT_URL}&?search=${name}`;
+  const url = `${ROOT_URL}?search=${name}`;
   const request = axios.get(url);
 
   return {
-    type: FETCH_SWAPI
+    type: FETCH_SWAPI,
     payload: request
   };
 }
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { connect} from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchSwapi } from '../actions/index';
 
-export default class SearchBar extends Component {
+
+class SearchBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = { term: ''};
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
@@ -16,6 +21,8 @@ export default class SearchBar extends Component {
 
   onFormSubmit(event) {
     event.preventDefault();
+    this.props.fetchSwapi(this.state.term);
+    this.setState({ term: '' });
   }
 
   render() {
@@ -33,3 +40,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchSwapi }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
