b778cd98c5e3fcdea570eeb42bd11a054b51c4ed
Awadje <michaelawad85@gmail.com>
2017-06-18 13:49:29 +0200

    fetching data

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import SearchBar from '../containers/search_bar';
+import SwapiList from '../containers/swapi_list';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <SearchBar />
+        <SwapiList />
       </div>
     );
   }
---
+++ src/containers/swapi_list.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect} from 'react-redux';
+
+class SwapiList extends Component {
+  renderSwapi(swapiData) {
+    return (
+      <tr>
+        <td>{swapiData.swapi.results}</td>
+      </tr>
+    );
+  }
+
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Birth Year</th>
+            <th>Height</th>
+            <th>Mass</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.swapi.results.map(this.renderSwapi)}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+function mapStateToProps({ swapi }){
+  return( { swapi });
+}
+
+export default connect(mapStateToProps)(SwapiList);
