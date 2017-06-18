df5f0b8e7dea78353b6df0aa7faac2593ae99006
Awadje <michaelawad85@gmail.com>
2017-06-18 14:59:05 +0200

    add console log

--- src/reducers/reducer_swapi.js
+++ src/reducers/reducer_swapi.js
@@ -1,6 +1,7 @@
 import { FETCH_SWAPI } from '../actions/index';
 
 export default function(state = [], action) {
+  console.log('Action received', action);
   switch(action.type) {
     case FETCH_SWAPI:
       return [ action.payload.data, ...state ];
