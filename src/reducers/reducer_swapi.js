43a039cd6a3e086cc5002efd900a223f356549c0
Awadje <michaelawad85@gmail.com>
2017-06-18 16:36:51 +0200

    fetching data

--- src/reducers/reducer_swapi.js
+++ src/reducers/reducer_swapi.js
@@ -3,7 +3,7 @@ import { FETCH_SWAPI } from '../actions/index';
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_SWAPI:
-      return [ action.payload.data.count.results, ...state ];
+      return [ action.payload.data, ...state ];
   }
   return state;
 }
