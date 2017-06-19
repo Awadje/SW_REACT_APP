879cfe0acea40f62b65bf337a5330e4c93e66fe8
Awadje <michaelawad85@gmail.com>
2017-06-19 17:31:25 +0200

    Minor Changes

--- src/reducers/reducer_swapi.js
+++ src/reducers/reducer_swapi.js
@@ -1,12 +1,10 @@
 import { FETCH_SWAPI } from '../actions/index';
 
 export default function(state = [], action) {
-
-    console.log('Action received', action);
-  switch (action.type) {
-
-    case FETCH_SWAPI:
-      return [ action.payload.data.results[0], ...state ];
-  }
+console.log('Action received', action);
+    switch (action.type) {
+      case FETCH_SWAPI:
+        return [ action.payload.data.results[0], ...state ];
+    }
   return state;
 }
