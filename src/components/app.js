0944b88599fc8d2276d0ef6541189df6a66d132c
Awadje <michaelawad85@gmail.com>
2017-06-19 11:21:20 +0200

    Styled searchfield and added button

--- src/components/app.js
+++ src/components/app.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 
 import SearchBar from '../containers/search_bar';
 import SwapiList from '../containers/swapi_list';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
-        <SwapiList />
+        <MuiThemeProvider>
+          <SearchBar />
+        </MuiThemeProvider>
+          <SwapiList />  
       </div>
     );
   }
